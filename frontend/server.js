43c9f62f044f0083a576e671d6c2bd430d8afc2b
jorgadev <radovanjorgic14@gmail.com>
2024-01-02 20:11:13 +0100

    erorr log

--- frontend/server.js
+++ frontend/server.js
@@ -4,16 +4,14 @@ const next = require("next");
 
 const dev = process.env.NODE_ENV !== "production";
 const hostname = "localhost";
-const port = 3000;
-// when using middleware `hostname` and `port` must be provided below
+const port = process.env.PORT || 3000;
+
 const app = next({ dev, hostname, port });
 const handle = app.getRequestHandler();
 
 app.prepare().then(() => {
   createServer(async (req, res) => {
     try {
-      // Be sure to pass `true` as the second argument to `url.parse`.
-      // This tells it to parse the query portion of the URL.
       const parsedUrl = parse(req.url, true);
       const { pathname, query } = parsedUrl;
 
@@ -29,12 +27,8 @@ app.prepare().then(() => {
       res.statusCode = 500;
       res.end("internal server error");
     }
-  })
-    .once("error", (err) => {
-      console.error(err);
-      process.exit(1);
-    })
-    .listen(port, () => {
-      console.log(`> Ready on http://${hostname}:${port}`);
-    });
+  }).listen(port, (err) => {
+    if (err) throw err;
+    console.log(`> Ready on http://${hostname}:${port}`);
+  });
 });
