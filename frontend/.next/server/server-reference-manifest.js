6ae4cf5f3efc31685270e95d97f4960c7ba781a9
jorgadev <radovanjorgic14@gmail.com>
2024-01-03 11:03:22 +0100

    build again

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"n/KtTjIO1iyBkLCJD/SwTJswsa+il3KrflzZnsbnkOM=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"iZGMkCHffueWuNZ8Dd6zDaBjlNr3cazjagmhg9otqaA=\"}"
