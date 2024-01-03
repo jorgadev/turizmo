28a680959d76bcb7b82b5f4b6ccd022d6913225e
jorgadev <radovanjorgic14@gmail.com>
2024-01-03 11:10:01 +0100

    develop test

--- frontend/.next/server/server-reference-manifest.js
+++ frontend/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"iZGMkCHffueWuNZ8Dd6zDaBjlNr3cazjagmhg9otqaA=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"qpxlWMJuQIFfqHgWeHbMveXbfbyK5qr7iR7EERXFUvQ=\"}"
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -4,12 +4,7 @@ import React from "react";
 export default function Page() {
   return (
     <div>
-      <Link href="/auth">
-        <div>Prijava za delavce test</div>
-      </Link>
-      <Link href="/auth">
-        <div>Prijava za podjetja develop branch</div>
-      </Link>
+      test develop
     </div>
   );
 }
