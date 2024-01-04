b8a15b560a006379b76453364c53fb7c0e8df645
jorgadev <radovanjorgic14@gmail.com>
2024-01-04 15:30:31 +0100

    frontend again

--- frontend/.next/static/v1MYvD4HjShiSJVVC78jB/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-836c9ddbda204558.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
