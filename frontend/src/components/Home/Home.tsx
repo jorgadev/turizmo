58a351ff320b8051602559e9e488c77f5ab9069c
jorgadev <radovanjorgic14@gmail.com>
2024-01-04 09:52:25 +0100

    frontend fix

--- frontend/src/components/Home/Home.tsx
+++
@@ -1,5 +0,0 @@
-import Button from "../shared/Button/Button";
-
-export default function Home() {
-  return <div>Home</div>;
-}
--- frontend/src/components/shared/Button/Button.tsx
+++
@@ -1,8 +0,0 @@
-type Props = {
-  title: string;
-  onClick: () => void;
-};
-
-export default function Button({ title = "Button", onClick }: Props) {
-  return <button onClick={onClick}>{title}</button>;
-}
